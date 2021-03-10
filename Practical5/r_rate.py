2771f08efdf6da1821fa3df2dbc91d16677b7336
ZhouJJ-ZJE <79961004+ZhouJJ-ZJE@users.noreply.github.com>
2021-03-10 15:03:43 +0800

    Update r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -3,6 +3,6 @@ r=1.2 #rate
 for i in range (1,10):
   n=n*r+n #one time of infect
   if i==5:
-    print ("the total number of individuals infected after 5 generations is" + str(n)) #print result
+    print ("the total number of individuals infected after 5 generations is " + str(n)) #print result
     break
   
