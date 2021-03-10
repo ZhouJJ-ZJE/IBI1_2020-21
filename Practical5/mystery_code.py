469fee15164f1d54b0d75ae87191f6c0e0ef5614
ZhouJJ-ZJE <79961004+ZhouJJ-ZJE@users.noreply.github.com>
2021-03-10 14:44:33 +0800

    Update mystery_code.py

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: 
+# Answer:Randomly generate numbers that greater than 1 and no larger than 50.
 
 # Import libraries
 # randint allows drawing a random number, 
@@ -7,10 +7,10 @@
 from random import randint
 
 # ceil takes the ceiling of a number, i.e. the next higher integer.
-# e.g. ceil(4.2)=5
+# e.g. ceil(4rm -rf.2)=5
 from math import ceil
 
-
+# randomly generarte numbers from 1 to 100 and only numbers no larger than 50 can be printed.
 p=False
 while p==False:
 	p = True
