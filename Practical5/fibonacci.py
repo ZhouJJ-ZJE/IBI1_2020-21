81566d2fc0a84935ac0d413c4aa25a32ef762ca7
ZhouJJ-ZJE <79961004+ZhouJJ-ZJE@users.noreply.github.com>
2021-03-10 14:09:55 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,1 +1,15 @@
-
+#a =1, b=1 , c= number= 2
+#for c from 0 to 14 , a = a+b,c=c+1,loop
+a=1
+b=1
+c=2
+d="th"
+for i in range(0,15):
+  a=a+b
+  c=c+1
+  print (str(c)+d+str(a))
+  b=a+b
+  c=c+1
+  print (str(c)+d+str(b))
+    if i==13:
+      break
