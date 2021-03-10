79f4960027098d4e6bc76a98bf5daad5cdaed4ae
ZhouJJ-ZJE <79961004+ZhouJJ-ZJE@users.noreply.github.com>
2021-03-10 15:34:08 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -5,11 +5,12 @@ b=1
 c=2
 print ("1st 1\n2nd 2")  #print the first and second number
 for i in range(0,15):
-  a=a+b #generate the next number
+  a=a+b #now a becomes the next nunmber
   c=c+1
   print (str(c)+"th "+str(a)) #print number
   b=a+b
   c=c+1
-  print (str(c)+"th "+str(b)) #again
+  print (str(c)+"th "+str(b)) #again, but this time b is the next number
+  #now, the first number a and second number b become separately the third and forth number， and repeat like this
   if i==13:
       break
