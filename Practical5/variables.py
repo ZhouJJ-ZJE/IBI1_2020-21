cee95e7cfb57079fdc707e8cc3641740b022c9bd
ZhouJJ-ZJE <79961004+ZhouJJ-ZJE@users.noreply.github.com>
2021-03-10 15:52:27 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,8 +3,15 @@ b = 260103
 c = 100321
 d = b - c
 e = b - a
-X = d>e
-Y = d<=e
-Z = (X and not Y) or (Y and not X)
-if X = true:
-  print "d is greater"
+if d>e:
+  print ("d is greater")
+if d<=e:
+  print ("e is greater")
+
+X=True
+Y=False
+Z=(X and not Y) or (Y  and not X)
+print (Z)
+
+W=X!=Y
+print(W==Z)
