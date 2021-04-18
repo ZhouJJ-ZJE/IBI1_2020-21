8001da083073213450f60e5bf8202ca7aa935e6e
ZhouJJ-ZJE <79961004+ZhouJJ-ZJE@users.noreply.github.com>
2021-04-18 22:03:22 +0800

    Update DNA_to_protein.py

--- Practical8/DNA_to_protein.py
+++ Practical8/DNA_to_protein.py
@@ -1,7 +1,6 @@
 import re
-def translate(sequence):
-    dict()
-    protein = {
+dict()
+protein = {
     'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L', 'TCT': 'S', 'TCC': 'S', 'TCG': 'S',
     'TAT': 'Y', 'TAC': 'Y', 'TAA': 'O', 'TAG': 'U', 'TGT': 'C', 'TGC': 'C', 'TGA': 'X', 'TGG': 'W',
     'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L', 'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
@@ -10,11 +9,9 @@ def translate(sequence):
     'AAT': 'N', 'AAC': 'B', 'AAA': 'K', 'AAG': 'K', 'AGT': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
     'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V', 'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
     'GAT': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E', 'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
-    }
-    pro = 'the encoded protein is '
-    for i in range(0, len(sequence), 3):
-        pro = pro + protein[sequence[i:i + 3]]
-    return pro
-
-sequence = 'ATGCGACTACGATCGAGGGCC'
-print(translate(sequence))
+}
+seq = 'ATGCGACTACGATCGAGGGCC'
+pro = 'the encoded protein is '
+for i in range(0, len(seq), 3):
+    pro = pro + protein[seq[i:i + 3]]
+print(pro)
